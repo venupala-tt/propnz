56aac7448dba8b0db952c64dac3c34d19283c50d
techtalentsindia <160840676+techtalentsindia@users.noreply.github.com>
2025-08-22 21:54:04 +0530

    Update route.ts

--- app/api/boecontact/route.ts
+++ app/api/boecontact/route.ts
@@ -9,7 +9,7 @@ export async function POST(req: Request) {
       service: "Gmail", // or any SMTP provider
       auth: {
         user: process.env.MY_EMAIL,
-        pass: process.env.MY_PASS,
+        pass: process.env.MY_PASSWORD,
       },
     });
 
