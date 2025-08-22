22583511ecf89732022d84a2deb2b13c19ab46d3
venupala-tt <venupala@techtalents.in>
2025-08-22 21:30:54 +0530

    Update page.tsx

--- app/pages/book-an-expert/page.tsx
+++ app/pages/book-an-expert/page.tsx
@@ -1,4 +1,4 @@
-import ContactForm from "../../components/boe-ContactForm";
+import ContactForm from "../../../components/boe-ContactForm";
 
 export default function BookAnExpertPage() {
   return (
