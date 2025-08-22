50c7ed0d60d1cad9cc65e980e9a68ab0934b0f29
techtalentsindia <160840676+techtalentsindia@users.noreply.github.com>
2025-08-22 21:49:59 +0530

    Update route.ts

--- app/api/boecontact/route.ts
+++ app/api/boecontact/route.ts
@@ -8,8 +8,8 @@ export async function POST(req: Request) {
     const transporter = nodemailer.createTransport({
       service: "Gmail", // or any SMTP provider
       auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
+        user: process.env.MY_EMAIL,
+        pass: process.env.MY_PASS,
       },
     });
 
