976acb279bd04f09f6178f653a1e8ef09752f33b
venupala-tt <venupala@techtalents.in>
2025-08-22 20:48:05 +0530

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -16,7 +16,7 @@ export default function Navbar() {
     { href: "/properties", label: "Properties" },
     { href: "/services", label: "Services" },
     { href: "/products", label: "Products" },
-    { href: "/roi", label: "ROI Calc" },
+    { href: "/bae", label: "Book Expert" },
     { href: "/blog", label: "Blogs" },
     { href: "/contact", label: "Contact" },
   ];
