9be4c5809403f27e3f9282ce0c8292954085513e
venupala-tt <venupala@techtalents.in>
2025-08-22 20:51:26 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -19,7 +19,7 @@ export default function Footer() {
           <ul className="space-y-2">
             <li>
               <Link
-                href="/pages/roi"
+                href="/components/roi"
                 className="text-gray-600 hover:text-blue-600 transition"
               >
                 ROI Calulator
