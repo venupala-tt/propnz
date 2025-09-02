1ef4d2dda1c6fa966dae9b8fe2b8529932454b68
venupala-tt <venupala@techtalents.in>
2025-09-02 15:51:06 +0530

    Update page.tsx

--- app/pages/tools/page.tsx
+++ app/pages/tools/page.tsx
@@ -15,7 +15,9 @@ export default function ToolsPage() {
           href="../pages/roi"
           className="block rounded-lg shadow-lg p-6 bg-gradient-to-r from-blue-600 to-purple-500 text-white hover:shadow-xl transition-all"
         >
-          <h2 className="text-lg font-semibold">Investing in Property?? visit our ROI Calculator</h2>
+          <h2 className="text-lg font-semibold">
+            Investing in Property?? visit our ROI Calculator
+          </h2>
         </Link>
 
         {/* Capital Gains Calculator */}
@@ -26,7 +28,7 @@ export default function ToolsPage() {
           <h2 className="text-lg font-semibold">Capital Gains Calculator</h2>
         </Link>
 
-           {/* Real Estate Unit Converter */}
+        {/* Real Estate Unit Converter */}
         <Link
           href="../pages/rcalc"
           className="block rounded-lg shadow-lg p-6 bg-gradient-to-r from-green-600 to-teal-500 text-white hover:shadow-xl transition-all"
@@ -34,12 +36,24 @@ export default function ToolsPage() {
           <h2 className="text-lg font-semibold">Real Estate Unit Converter</h2>
         </Link>
 
-          {/* Flats Development ROI Calculator for Land Owner */}
+        {/* Flats Development ROI Calculator for Land Owner */}
         <Link
           href="../pages/fdroi"
           className="block rounded-lg shadow-lg p-6 bg-gradient-to-r from-green-600 to-teal-500 text-white hover:shadow-xl transition-all"
         >
-          <h2 className="text-lg font-semibold">Flats Development ROI Calculator for Land Owner</h2>
+          <h2 className="text-lg font-semibold">
+            Flats Development ROI Calculator for Land Owner
+          </h2>
+        </Link>
+
+        {/* Single Unit ROI Calculator */}
+        <Link
+          href="../pages/suroi"
+          className="block rounded-lg shadow-lg p-6 bg-gradient-to-r from-indigo-600 to-blue-500 text-white hover:shadow-xl transition-all"
+        >
+          <h2 className="text-lg font-semibold">
+            Single Unit ROI Calculator
+          </h2>
         </Link>
 
         {/* More Tools */}
