888602b7c39d7b6cb34e96d6fc32c0da5cc4b467
venupala-tt <venupala@techtalents.in>
2025-09-03 15:12:10 +0530

    Update page.tsx

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -57,12 +57,14 @@ export default function ServicesPage() {
       >
         {/* Top-right link */}
         <div className="flex justify-end mb-6">
-          <Link
-            href="../pages/book-an-expert"
-            className="text-blue-600 font-semibold hover:underline"
-          >
-            Book Our Expert
-          </Link>
+       <Link
+              href="/pages/book-an-expert"
+              className="px-6 py-3 rounded-xl shadow-lg font-semibold text-white 
+                         bg-gradient-to-r from-blue-600 via-purple-500 to-pink-500 
+                         hover:opacity-90 transition text-center sm:text-left"
+            >
+              Book Our Expert
+            </Link>
         </div>
         <h2
           className="text-3xl sm:text-4xl font-bold text-center mb-12 
