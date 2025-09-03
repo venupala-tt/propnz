c9b1c7294490423905e9cbf6ec0ada3756f8128e
venupala-tt <venupala@techtalents.in>
2025-09-03 23:25:04 +0530

    Update route.ts

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import client from "../../../lib/contentful";
+import client from "lib/contentful";
 
 export async function GET(req: Request) {
   const { searchParams } = new URL(req.url);
