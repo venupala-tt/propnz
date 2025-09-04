db37498406486293130725e9eb13e3f293d1454b
venupala-tt <venupala@techtalents.in>
2025-09-04 12:45:50 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import Link from "next/link";
 import { Package, Wrench, FileText } from "lucide-react";
 import dynamic from "next/dynamic";
- 
+import SearchBar from "../components/SearchBar";
+
 
 export default function HomePage() {
   return (
@@ -12,7 +13,9 @@ export default function HomePage() {
         bg-gradient-to-br from-blue-100 via-white to-purple-100 
         animate-gradientWave"
       >
-         {/* Hero Section */}
+         <h1 className="text-2xl font-bold mb-6">Property Search</h1>
+      <SearchBar />
+        {/* Hero Section */}
         <div
           className="w-full max-w-4xl rounded-xl shadow-md 
           bg-white/80 backdrop-blur-sm p-6 sm:p-8 
