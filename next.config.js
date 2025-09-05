ff8abe7540f7aba198dbe82b75601bc17882bd00
venupala-tt <venupala@techtalents.in>
2025-09-05 11:44:57 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["images.ctfassets.net"], // ✅ allow Contentful images
+  },
+};
+
+module.exports = nextConfig;
