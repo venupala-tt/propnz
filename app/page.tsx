38c1bbb28f3b0dac54012cc272f21a6f7c4ce296
venupala-tt <venupala@techtalents.in>
2025-09-05 17:24:39 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default function HomePage() {
 <div  className="w-full max-w-5xl rounded-xl shadow-md  bg-white/80 backdrop-blur-sm p-4 sm:p-6 animate-fadeInBounce flex flex-col sm:flex-row items-center sm:items-start sm:justify-between gap-4"
 >         
             <h2 className="text-lg font-semibold mb-2">
-              Learn & Build scalable, property solutions that are performant and visually appealing 
+              Learn & Build scalable, property solutions <br> that are performant and visually appealing </br>
             </h2>
             <Link
               href="/pages/book-an-expert"
