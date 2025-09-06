6460fbf64ccf5adccdb51eb5e1bf2530a4a07339
venupala-tt <venupala@techtalents.in>
2025-09-06 18:18:13 +0530

    Create route.ts

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,34 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+import db from "@/lib/mongodb";
+import User from "@/models/User";
+
+const handler = NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        await db;
+        const user = await User.findOne({ email: credentials?.email });
+        if (!user) return null;
+
+        const isValid = await bcrypt.compare(
+          credentials!.password,
+          user.password
+        );
+        if (!isValid) return null;
+
+        return { id: user._id.toString(), name: user.name, email: user.email };
+      },
+    }),
+  ],
+  session: { strategy: "jwt" },
+  secret: process.env.NEXTAUTH_SECRET,
+});
+
+export { handler as GET, handler as POST };
