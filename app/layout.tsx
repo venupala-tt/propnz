4a58fb3d8c3e12af637504fdb0920121d17866dc
venupala-tt <venupala@techtalents.in>
2025-09-07 11:54:28 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Providers } from "../providers";
+import { Providers } from "/app/providers";
 
 export const metadata: Metadata = {
   title: "Propmatics",
