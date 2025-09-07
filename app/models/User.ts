49e6a974f98a7bb00e06ccc11f6628a95b2b75ba
venupala-tt <venupala@techtalents.in>
2025-09-07 13:12:07 +0530

    Update User.ts

--- app/models/User.ts
+++ app/models/User.ts
@@ -1,9 +1,14 @@
-import mongoose from "mongoose";
+import mongoose, { Schema, models } from "mongoose";
 
-const UserSchema = new mongoose.Schema({
-  name: String,
-  email: { type: String, unique: true },
-  password: String,
-});
+const UserSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
-export default mongoose.models.User || mongoose.model("User", UserSchema);
+const User = models.User || mongoose.model("User", UserSchema);
+
+export default User;
