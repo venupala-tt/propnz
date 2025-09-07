efbf6f0ddd0d56430c20f5419f9d37888f450f70
venupala-tt <venupala@techtalents.in>
2025-09-07 15:57:37 +0530

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,8 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import { connectDB } from "/app/lib/mongodb";
-import User from "/app/models/User";
+import { connectDB } from "@/lib/mongodb";
+import User from "@/models/User";
+
 import bcrypt from "bcrypt";
 
 const handler = NextAuth({
