54ffef99d485da7b0d6a2a7eef8a91c611843b21
venupala-tt <venupala@techtalents.in>
2025-09-07 15:29:38 +0530

    Update route.ts

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import bcrypt from "bcrypt";
-import { connectDB } from "@/lib/mongodb";
+import { connectDB } from "/app/lib/mongodb";
 import User from "@/models/User";
 
 export async function POST(req: Request) {
