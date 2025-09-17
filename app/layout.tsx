3802a32bb30bb10d4edae095c3118d94c6ed1975
venupala-tt <venupala@techtalents.in>
2025-09-17 13:01:31 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           <Navbar />
           {children}
         </Providers>
+        </Footer>
       </body>
     </html>
   );
