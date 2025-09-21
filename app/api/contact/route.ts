971bf231f73ec2de83219939b71642ef1329cf9e
venupala-tt <venupala@techtalents.in>
2025-09-21 20:46:36 +0530

    Update route.ts

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -11,13 +11,16 @@ export async function POST(req: Request) {
       port: Number(process.env.EMAIL_SERVER_PORT),
       secure: Number(process.env.EMAIL_SERVER_PORT) === 465, // ✅ auto secure for 465
       auth: {
-        user: process.env.EMAIL_SERVER_USER,
-        pass: process.env.EMAIL_SERVER_PASSWORD,
+ //       user: process.env.EMAIL_SERVER_USER,
+   //     pass: process.env.EMAIL_SERVER_PASSWORD,
+        user: "info@propmatics.com",
+        pass: "Thiru@987$_ap",
       },
     });
 
     await transporter.sendMail({
-      from: `"${name}" <${process.env.EMAIL_FROM}>`,
+      // from: `"${name}" <${process.env.EMAIL_FROM}>`,
+      from: `"Propmatics" <info@propmatics.com>`,
       to: process.env.CONTACT_RECEIVER,
       subject: `New Contact Form Message from ${name}`,
       text: `You got a new message from:\n\nName: ${name}\nEmail: ${email}\n\nMessage:\n${message}`,
