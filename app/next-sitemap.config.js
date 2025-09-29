5a03adfb56879bb9ee12e39b378cafdb93453b8b
venupala-tt <venupala@techtalents.in>
2025-09-29 12:57:35 +0530

    Update next-sitemap.config.js

--- app/next-sitemap.config.js
+++ app/next-sitemap.config.js
@@ -1,4 +1,7 @@
 module.exports = {
-siteUrl: "https://www.propmatics.com",
-generateRobotsTxt: true,
+  siteUrl: "https://www.propmatics.com",
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  changefreq: "weekly",
+  priority: 0.7,
 };
